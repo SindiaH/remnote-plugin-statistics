2e12bf87dac70d47fdc82cb96a67e01ff05954e5
Sindia Höller <sindia.hoeller@outlook.com>
2024-12-01 22:33:06 +0100

    Tested new feature

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -1,7 +1,52 @@
-import { AppEvents, declareIndexPlugin, ReactRNPlugin, RNPlugin, WidgetLocation } from '@remnote/plugin-sdk';
+import {
+  AppEvents,
+  declareIndexPlugin,
+  PluginCommandMenuLocation,
+  ReactRNPlugin,
+  RNPlugin,
+  usePlugin,
+  WidgetLocation,
+} from '@remnote/plugin-sdk';
 import '../../public/App.css';
+import { Command } from 'concurrently';
 
 async function onActivate(plugin: ReactRNPlugin) {
+  console.log('Plugin activated');
+
+  await plugin.app.registerMenuItem({
+    id: "reset-all-cards",
+    name: "Reset All Cards",
+    location: PluginCommandMenuLocation.DocumentMenu,
+    // iconUrl: 'https://png.pngtree.com/element_our/20190530/ourmid/pngtree-correct-icon-image_1267804.jpg',
+    action: async (document) => {
+      console.log('Sending message', document.remId);
+
+      let parentRem = await plugin.rem.findOne(document.remId);
+      let rems = await parentRem?.getChildrenRem();
+
+      console.log(rems);
+      // let rems = await plugin.rem.findMany(context);
+      const nextPracticeTime = (new Date()).getTime();
+
+      rems?.forEach((rem) => {
+        rem.getType().then((type) => {
+          console.log('type', type);
+        });
+        rem?.getCards().then((cards) => {
+          cards?.forEach((card) => {
+            console.log('Remove card');
+
+            // card.update({h: [], n: nextPracticeTime, a: nextPracticeTime, t: 0, l: undefined});
+            console.log(card._id, card.type);
+            // card.updateCardRepetitionStatus();
+          });
+        });
+
+
+      })
+    },
+  });
+
   await plugin.settings.registerBooleanSetting({
     id: 'ShowStatisticsForCard',
     title: 'Show the statistics indicators for a card while learning',
@@ -44,6 +89,20 @@ async function onActivate(plugin: ReactRNPlugin) {
         },
       },
     );
+  }
+
+  async function resetFocusedCard() {
+
+
+
+    // const rId = rem.parent;
+    // const nextPracticeTime = (new Date()).getTime();
+    // const cards = window.CardCollection(window.currentKnowledgeBaseId());
+    // console.log(rId);
+    // const card = cards.findOne({remId: rId});
+    // console.log("Card:", cards, card, rId);
+    // card.update({h: [], n: nextPracticeTime, a: nextPracticeTime, t: 0, l: undefined});
+    // console.info("Reset Schedule of rem", rem.key, "with id", rId);
   }
 
 }
